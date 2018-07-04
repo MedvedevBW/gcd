af2f20c0c9fa89474730feb616912c5cc5102882
Vladislav Medvedev <medvedev_vv@bw-sw.com>
2018-07-04 17:46:52 +0700

    master import was optimized

--- src/main/java/com/bwsw/test/gcd/GcdApplication.java
+++ src/main/java/com/bwsw/test/gcd/GcdApplication.java
@@ -1,29 +1,23 @@
 package com.bwsw.test.gcd;
 
 import com.bwsw.test.gcd.configuration.AppConfig;
-import com.bwsw.test.gcd.services.GcdService;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 
 @EnableRabbit
 @SpringBootApplication
-public class GcdApplication extends SpringBootServletInitializer /*implements RabbitListenerConfigurer */{
+public class GcdApplication extends SpringBootServletInitializer {
 
 	@Autowired
 	private AppConfig config;
--- src/main/java/com/bwsw/test/gcd/controllers/GcdController.java
+++ src/main/java/com/bwsw/test/gcd/controllers/GcdController.java
@@ -1,19 +1,12 @@
 package com.bwsw.test.gcd.controllers;
 
-import com.bwsw.test.gcd.configuration.AppConfig;
-import com.bwsw.test.gcd.entities.*;
-import com.bwsw.test.gcd.rabbitmq.Producer;
-import com.bwsw.test.gcd.repositories.GcdArgumentsRepository;
-import com.bwsw.test.gcd.repositories.GcdResultRepository;
+import com.bwsw.test.gcd.entities.GcdArgumentsApi;
+import com.bwsw.test.gcd.entities.GcdResult;
 import com.bwsw.test.gcd.services.GcdService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 public class GcdController {
